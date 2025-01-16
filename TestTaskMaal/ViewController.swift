d740bf18c5d1065d5141217ebf25cb638b27aa83
Niff1e <malanka.kering@gmail.com>
2025-01-16 13:44:09 +0300

    Initial Commit

---
+++ TestTaskMaal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskMaal
+//
+//  Created by Pavel Maal on 16.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
